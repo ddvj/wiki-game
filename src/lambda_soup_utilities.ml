4d8aa1c583c2f3cf9f0551cdc64b9271a5cde07d
Derrick <deucevincent@icloud.com>
2025-06-23 19:45:25 +0000

    implemented the bold function, haven't added a test

--- src/lambda_soup_utilities.ml
+++ src/lambda_soup_utilities.ml
@@ -111,8 +111,11 @@ let%expect_test "get_first_item_of_second_unordered_list" =
 
 (* Gets all bolded text from an HTML page. *)
 let get_bolded_text contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  let open Soup in
+  parse contents
+  $$ "b"
+  |> to_list
+  |> List.map ~f:(fun b -> texts b |> String.concat ~sep:"" |> String.strip)
 ;;
 
 (* [make_command ~summary ~f] is a helper function that builds a simple HTML parsing
